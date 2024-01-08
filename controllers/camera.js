aceb0a4d8e60e392a3ddfc36c28d187c841af8b4
Rida Messaoudene <rida.messaoudene@gmail.com>
2024-01-08 15:15:56 +0100

    init project

---
+++ controllers/camera.js
@@ -0,0 +1,24 @@
+const uuid = require('uuid/v1');
+const Camera = require('../models/Camera');
+
+exports.getAllCameras = (req, res, next) => {
+};
+
+exports.getOneCamera = (req, res, next) => {
+};
+
+/**
+ *
+ * Expects request to contain:
+ * contact: {
+ *   firstName: string,
+ *   lastName: string,
+ *   address: string,
+ *   city: string,
+ *   email: string
+ * }
+ * products: [string] <-- array of product _id
+ *
+ */
+exports.orderCameras = (req, res, next) => {
+};
